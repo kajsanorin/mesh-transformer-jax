c094657a620f2925033bdf8e5a2d674b4d2de026
Kajsa Norin <kajsa.norin@gmail.com>
2022-03-23 16:36:07 +0100

    use correct file path

--- mesh_transformer/checkpoint.py
+++ mesh_transformer/checkpoint.py
@@ -35,7 +35,7 @@ def write(x, ckpt_dir):
     # start = time.time()
     idx, i = x
     # file_path = ckpt_dir + f"{idx}.npz"
-    file_path = '/home/kajsa/data' + f"{idx}.npz"
+    file_path = '/home/kajsa/data/' + f"{idx}.npz"
     # for _ in range(3):
         # with open(file_path, "wb") as f:
     with open(file_path, "wb") as f:
